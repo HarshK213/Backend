927ed2b6e08c53cec294b9907e674413afdd0d6d
HarshK213 <hkushwaha21305@gmail.com>
2025-07-27 18:57:22 +0530

    feat: add models for comments, likes, playlists, and tweets with mongoose schemas

---
+++ 4_BackendSetup/src/models/comment.models.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
+
+const commentSchema = mongoose.Schema(
+     {
+          content : {
+               type : String,
+               required : true,
+          },
+          owner : {
+               type : mongoose.Schema.Types.ObjectId,
+               ref : "User"
+          },
+          video : {
+               type : mongoose.Schema.Types.ObjectId,
+               ref : "Video"
+          }
+     },
+     {
+          timestamps : true
+     }  
+);
+
+commentSchema.plugin(mongooseAggregatePaginate)
+
+export const Comment = mongoose.model("Comment", commentSchema);
